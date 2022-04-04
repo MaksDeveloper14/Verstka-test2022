df150c5cc0d60fe8dc8ff292a0d5ded808d32e05
Маккси <Maksim-volkov1994@yandex.ru>
2022-04-05 00:14:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Узнаем по какому элементу кликнули через делегирование событий и выполняем действие 
+// document.addEventListener('click', TransparentElement);
+// function TransparentElement(e) {
+//   e.target.style.opacity = 0;
+// }
+
+
+
+
+
